5aa28f70d32025973add45627f2ebbb2f1475cca
Hiram <hiram.habib@soeidental.com>
2023-01-11 15:06:08 +1300

    implement the random names with tests

--- lib/mimik/names.rb
+++ lib/mimik/names.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 module Mimik
   module Names
+    dir = "#{__dir__}/locales"
+    @@localised_names = YAML.load_file("#{dir}/names.yml")
+
     def first_name_male
-      p __dir__
+      @@localised_names["male_first_name"].sample
+    end
+
+    def first_name_female
+      @@localised_names["female_first_name"].sample
+    end
+
+    def last_name
+      @@localised_names["last_name"].sample
     end
   end
 end
--- spec/random_spec.rb
+++ spec/random_spec.rb
@@ -5,8 +5,15 @@ RSpec.describe Random do
     expect(Random.boolean).to be_boolean
   end
 
-  it "should generate a random first name" do
-    p Random.first_name_male
-    true
+  it "should generate a random male first name" do
+    expect(Random.first_name_male).to be_a(String)
+  end
+
+  it "should generate a random female first name" do
+    expect(Random.first_name_female).to be_a(String)
+  end
+
+  it "should generate a random last name" do
+    expect(Random.last_name).to be_a(String)
   end
 end
